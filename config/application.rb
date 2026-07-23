0c8574f45c4337ba9d17f75921680cabe23443a0
sameer siruguri <sameers.public@gmail.com>
2026-07-22 22:06:56 -0700

    add logs

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module EverythingApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    config.logger = ActiveSupport::Logger.new("#{Rails.root}/log/#{Rails.env}.log")
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
